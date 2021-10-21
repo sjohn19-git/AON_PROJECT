d341e86efb622aaa6f61d7879888e4d04f08e1b1
Sebin John <sjohn@ermine.giseis.alaska.edu>
2021-10-21 09:48:45 -0800

    crosse_correlator second commit

--- codes/cross_correlator.py
+++ codes/cross_correlator.py
@@ -1,6 +1,23 @@
 import obspy
 from obspy import read 
+import numpy as np
+import obspy.signal.cross_correlation as cr
+
 
 st=read("/home/sjohn/AON_PROJECT/O14K/O14K_2020-05-18T00:00:00.000000Z2020-12-04T00:00:00.000000Z.MSEED")
-st
-st[0].plot()
+tr1=st[0]
+tr2=st[1]
+tr1=tr1.data
+tr2=tr.data
+len(tr1)
+j=0
+corre=np.array([])
+for i in range(len(tr1)):
+    k=j+24
+    tr1par=tr1[j:k]
+    tr2par=tr2[j:k]
+    j=k
+    coeif=cr.xcorr(tr1par, tr2par,1)
+    corre=np.append(corre,np.array(coeif[1]))
+    
+tr1
